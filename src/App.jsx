f9f61f0babdb8b02d9fe4c3d910d3d138c6f41e5
unknown <omerme13@gmail.com>
2022-03-26 21:18:57 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,79 @@
+import React, { useEffect, useState } from "react";
+
+import Form from "./components/Form/Form";
+
+import "./App.scss";
+
+const App = () => {
+    const [testDiagnose, setTestDiagnose] = useState({});
+    const [error, setError] = useState(null);
+    const [isErrorShown, setIsErrorShown] = useState(false);
+    
+    const isContentShown = testDiagnose.data || testDiagnose.isLoading;
+
+    const getContent = () => {
+        if (testDiagnose.isLoading) {
+            return "Loading...";
+        }
+
+        return testDiagnose.isFound ? (
+            <div>
+                <div className="diagnose__content-name">
+                    {testDiagnose.data.name}
+                </div>
+                <br />
+                <div>
+                    {testDiagnose.data.isGoodDiagnose ? "Good 🙂" : "Bad 🙁"}
+                </div>
+            </div>
+        ) : (
+            testDiagnose.data.name // name is unknown
+        );
+    };
+
+    const getContentClass = () => {
+        if (!testDiagnose.data || testDiagnose.isLoading) {
+            return;
+        }
+
+        if (!testDiagnose.isFound) {
+            return "unknown";
+        }
+
+        return testDiagnose.data.isGoodDiagnose ? "good" : "bad";
+    };
+
+    useEffect(() => {
+        if (error) {
+            setIsErrorShown(true);
+            let timer = setTimeout(() => setIsErrorShown(false), 3000);
+
+            return () => {
+                clearTimeout(timer);
+            };
+        }
+    }, [error, setIsErrorShown]);
+
+    return (
+        <div className="app">
+            <h1 className="app__header">Health App</h1>
+            <div className="app__content">
+                <Form setTestDiagnose={setTestDiagnose} setError={setError} />
+                <div className="diagnose">
+                    {isContentShown && (
+                        <div
+                            className={`diagnose__content diagnose__content--${getContentClass()}`}
+                        >
+                            {getContent()}
+                        </div>
+                    )}
+                </div>
+            </div>
+            {isErrorShown && (
+                <div className="app__error">{error.response.data.message}</div>
+            )}
+        </div>
+    );
+};
+
+export default App;
---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,72 @@
+import React, { useEffect, useState } from "react";
+import useAxios from "axios-hooks";
+
+import "./Form.scss";
+
+const Form = ({ setTestDiagnose, setError }) => {
+    const [testName, setTestName] = useState("");
+    const [testResult, setTestResult] = useState(0);
+
+    const isFormValid = testName && testResult;
+
+    const [{ data, loading, error }, getTestResult] = useAxios(
+        {
+            url: "http://localhost:3000/api/v1/tests/test",
+            method: "POST",
+            data: { testName, userTestResult: testResult },
+        },
+        { manual: true }
+    );
+
+    const handleSendButton = () => {
+        if (isFormValid) {
+            getTestResult();
+        }
+    };
+
+    const handleSendOnEnter = e => {
+        if (e.key === "Enter") {
+            handleSendButton();
+        }
+    };
+
+    useEffect(() => {
+        setTestDiagnose({ ...data, isLoading: loading });
+    }, [data, setTestDiagnose, loading]);
+
+    useEffect(() => {
+        if (error) {
+            setError(error)
+        }
+    }, [setError, error]);
+
+    return (
+        <div className="form" onKeyDown={handleSendOnEnter}>
+            <div className="form__input-container">
+                <input
+                    type="text"
+                    className="form__input"
+                    value={testName}
+                    onChange={e => setTestName(e.target.value)}
+                    placeholder="Enter test's name"
+                />
+                <input
+                    type="number"
+                    className="form__input"
+                    value={testResult || ""}
+                    onChange={e => setTestResult(e.target.value)}
+                    placeholder="Enter test's result"
+                />
+            </div>
+            <button
+                className="form__button"
+                onClick={handleSendButton}
+                disabled={!isFormValid}
+            >
+                Send
+            </button>
+        </div>
+    );
+};
+
+export default Form;
